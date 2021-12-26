d25b1372ae4b866a606209b81e60eedd0cacc51a
BWsix <itisbw62@gmail.com>
2021-12-26 14:12:24 +0800

    feat: added yargs & cli login

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("bob");
+#!/usr/bin/env node
+
+import yargs from "yargs";
+
+yargs
+  .scriptName("clcp-spotify")
+  .usage("$0 <cmd> [args]")
+  .command(
+    "login [id] [secret]",
+    "Login before using this app.",
+    (yargs) => {
+      yargs
+        .positional("id", {
+          type: "string",
+          describe: "your spotify app client id",
+        })
+        .positional("secret", {
+          type: "string",
+          describe: "your spotify app client secret",
+        });
+    },
+    async (argv) => {
+      const { login } = await import("./login");
+
+      const clientId = argv.id as string;
+      const clientSecret = argv.secret as string;
+
+      login({ clientId, clientSecret });
+    },
+  )
+  .help().argv;
